61847c5b24b1229d59f5cb1b88a323ce95f4dff0
peddrums <pietromineo87@gmail.com>
2021-01-21 15:22:57 +0000

    Script annotations

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+// Array imports
 const { nouns } = require('./nouns');
 const { verbs } = require('./verbs');
 const { adjectives } = require('./adjectives');
 
+// Index generator
 let randomNumber = arr => {
     return Math.floor(Math.random()*arr.length)
 }
 
+// MadLib generator
 const madlib = nome => {
     let noun = nouns[randomNumber(nouns)];
     let verb = verbs[randomNumber(verbs)];
@@ -13,7 +16,7 @@ const madlib = nome => {
     console.log(`${verb[0].toUpperCase()+verb.substring(1)} ${nome[0].toUpperCase()+nome.substring(1)}, the ${adjective} ${noun}!`);
 }
 
-
+// User input
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
